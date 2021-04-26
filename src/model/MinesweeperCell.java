6f238791bc2809eca8fe83edfa15b519920d8f4b
Derek <dtominaga72@gmail.com>
2021-04-26 10:36:45 -0700

    Cell Constructor

--- src/model/MinesweeperCell.java
+++ src/model/MinesweeperCell.java
@@ -4,9 +4,14 @@ public class MinesweeperCell {
 	
 	private boolean hidden;
 	private boolean mined;
+	private boolean flagged;
+	private int adjacentMines;
 	
 	public MinesweeperCell() {
-		
+		hidden = true;
+		minded = false;
+		falgged = false;
+		adjcentMines = null;
 	}
 
 }
